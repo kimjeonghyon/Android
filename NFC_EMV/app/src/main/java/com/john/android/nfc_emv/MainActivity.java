6064d42ae8876e3c595d15589a5ca0edf908d764
kimjeonghyon <kimjeonghyon@gmail.com>
2017-01-13 10:02:34 +0900

    Add files via upload

---
+++ NFC_EMV/app/src/main/java/com/john/android/nfc_emv/MainActivity.java
@@ -0,0 +1,262 @@
+package com.john.android.nfc_emv;
+
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.john.android.nfc_emv.records.ParsedNdefRecord;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final DateFormat TIME_FORMAT = SimpleDateFormat.getDateTimeInstance();
+    private LinearLayout mTagContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mTagContent = (LinearLayout) findViewById(R.id.list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear) {
+            for (int i = mTagContent.getChildCount() -1; i >= 0 ; i--) {
+                    mTagContent.removeViewAt(i);
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onResume() {
+        super.onResume();
+
+        resolveIntent(getIntent());
+    }
+
+
+    private void resolveIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+                Toast.makeText(this,"Unknow tag type",Toast.LENGTH_LONG).show();
+                byte[] empty = new byte[0];
+                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+                Parcelable tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                byte[] payload = dumpTagData(tag).getBytes();
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] { record });
+                msgs = new NdefMessage[] { msg };
+            }
+            // Setup the views
+            buildTagViews(msgs);
+        }
+    }
+
+
+    void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) {
+            Toast.makeText(this,"msgs is null",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        LayoutInflater inflater = LayoutInflater.from(this);
+        LinearLayout content = mTagContent;
+
+        // Parse the first message in the list
+        // Build views for all of the sub records
+        Date now = new Date();
+        List<ParsedNdefRecord> records = NdefMessageParser.parse(msgs[0]);
+        final int size = records.size();
+        for (int i = 0; i < size; i++) {
+            TextView timeView = new TextView(this);
+            timeView.setText(TIME_FORMAT.format(now));
+            content.addView(timeView, 0);
+            ParsedNdefRecord record = records.get(i);
+            content.addView(record.getView(this, inflater, content, i), 1 + i);
+            content.addView(inflater.inflate(R.layout.tag_divider, content, false), 2 + i);
+        }
+        if(msgs.length > 1) {
+            List<ParsedNdefRecord> records2 = NdefMessageParser.parse(msgs[1]);
+            final int size2 = records2.size();
+            for (int i = 0; i < size2; i++) {
+                TextView timeView2 = new TextView(this);
+                timeView2.setText(TIME_FORMAT.format(now));
+                content.addView(timeView2, 0);
+                ParsedNdefRecord record2 = records2.get(i);
+                content.addView(record2.getView(this, inflater, content, i), 1 + i);
+                content.addView(inflater.inflate(R.layout.tag_divider, content, false), 2 + i);
+            }
+        }
+    }
+    private String dumpTagData(Parcelable p) {
+        StringBuilder sb = new StringBuilder();
+        Tag tag = (Tag) p;
+        byte[] id = tag.getId();
+        sb.append("Tag ID (hex): ").append(getHex(id)).append("\n");
+        sb.append("Tag ID (dec): ").append(getDec(id)).append("\n");
+        sb.append("ID (reversed): ").append(getReversed(id)).append("\n");
+
+        String prefix = "android.nfc.tech.";
+        sb.append("Technologies: ");
+        for (String tech : tag.getTechList()) {
+            sb.append(tech.substring(prefix.length()));
+            sb.append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        for (String tech : tag.getTechList()) {
+            if (tech.equals(MifareClassic.class.getName())) {
+                sb.append('\n');
+                MifareClassic mifareTag = MifareClassic.get(tag);
+                String type = "Unknown";
+                switch (mifareTag.getType()) {
+                    case MifareClassic.TYPE_CLASSIC:
+                        type = "Classic";
+                        break;
+                    case MifareClassic.TYPE_PLUS:
+                        type = "Plus";
+                        break;
+                    case MifareClassic.TYPE_PRO:
+                        type = "Pro";
+                        break;
+                }
+                sb.append("Mifare Classic type: ");
+                sb.append(type);
+                sb.append('\n');
+
+                sb.append("Mifare size: ");
+                sb.append(mifareTag.getSize() + " bytes");
+                sb.append('\n');
+
+                sb.append("Mifare sectors: ");
+                sb.append(mifareTag.getSectorCount());
+                sb.append('\n');
+
+                sb.append("Mifare blocks: ");
+                sb.append(mifareTag.getBlockCount());
+            }
+
+            if (tech.equals(MifareUltralight.class.getName())) {
+                sb.append('\n');
+                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
+                String type = "Unknown";
+                switch (mifareUlTag.getType()) {
+                    case MifareUltralight.TYPE_ULTRALIGHT:
+                        type = "Ultralight";
+                        break;
+                    case MifareUltralight.TYPE_ULTRALIGHT_C:
+                        type = "Ultralight C";
+                        break;
+                }
+                sb.append("Mifare Ultralight type: ");
+                sb.append(type);
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+    private String getHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            int b = bytes[i] & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b));
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+    private long getDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = 0; i < bytes.length; ++i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+    private long getReversed(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+        resolveIntent(intent);
+    }
+
+}
+
+
+
